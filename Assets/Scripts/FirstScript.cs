540897e72fee2c9ac8d792c8a3f7b0320d4a5414
Sean Erren <ErrenSean@gmail.com>
2026-01-11 14:02:13 -0500

    Split speed to forward and backward variables. Added a boolean for direction. Made FirstScript variables public. Added a triangle to the scene using FirstScript.

--- Assets/Scripts/FirstScript.cs
+++ Assets/Scripts/FirstScript.cs
@@ -2,7 +2,10 @@ using UnityEngine;
 
 public class FirstScript : MonoBehaviour
 {
-    float speed = 0.01f;
+    public float forwardSpeed = 0.01f;
+    public float backwardSpeed = -0.01f;
+
+    public bool isForward = true;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -13,12 +16,22 @@ public class FirstScript : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        //Create a temporary Vector2 for the transform's position
         Vector2 newPos = transform.position, screenPos = Camera.main.WorldToScreenPoint(transform.position);
+
+        //Change direction if the edge of the screen is reached
         if (screenPos.x < 0)
-            speed = 0.01f;
+            isForward = true;
         if (screenPos.x > Screen.width)
-            speed = -0.1f;
-        newPos.x += speed;
+            isForward = false;
+
+        //Move according to the direction
+        if (isForward)
+            newPos.x += forwardSpeed;
+        else
+            newPos.x += backwardSpeed;
+
+        //Set the new transform position to the temporary Vector2
         transform.position = newPos;
     }
 }
