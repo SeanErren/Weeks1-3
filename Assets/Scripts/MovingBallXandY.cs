0cc146524c40eb55d093a8dae26d2362e38d0fb0
Sean Erren <ErrenSean@gmail.com>
2026-01-15 17:29:53 -0500

    Made DVD move based on time instead of frames for consistency

--- Assets/Scripts/MovingBallXandY.cs
+++ Assets/Scripts/MovingBallXandY.cs
@@ -2,7 +2,8 @@ using UnityEngine;
 
 public class MovingBallXandY : MonoBehaviour
 {
-    public float speedX = 0.1f, speedY = 0.1f;
+    //The movement happens in pixels
+    public int speedX = 50, speedY = 50;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -17,14 +18,31 @@ public class MovingBallXandY : MonoBehaviour
         //Getting the screen's position in pixels
         Vector2 posOnScreen = Camera.main.WorldToScreenPoint(transform.position);
 
-        posOnScreen.x += speedX;
-        posOnScreen.y += speedY;
+        //Up the position by the speed * the time that passed (so that it's consistent with the time)
+        posOnScreen.x += speedX * Time.deltaTime;
+        posOnScreen.y += speedY * Time.deltaTime;
 
         //Flip direction if X
         if (posOnScreen.x < 0 || posOnScreen.x > Screen.width)
+        {
+            //Checking if x has reached the edges, if so, set the value to that edge (to not go beyond it)
+            if (posOnScreen.x < 0)
+                posOnScreen.x = 0;
+            if (posOnScreen.x > Screen.width)
+                posOnScreen.x = Screen.width;
+
             speedX *= -1;
+        }
         if (posOnScreen.y < 0 || posOnScreen.y > Screen.height)
+        {
+            //Checking if y has reached the edges, if so, set the value to that edge (to not go beyond it)
+            if (posOnScreen.y < 0)
+                posOnScreen.y = 0;
+            if (posOnScreen.y > Screen.height)
+                posOnScreen.y = Screen.height;
+
             speedY *= -1;
+        }
 
         //Returning the position back to meter units
         posOnScreen = Camera.main.ScreenToWorldPoint(posOnScreen);
