e18fa4dc7dd929d09e017f276432b7a67838f27e
Sean Erren <ErrenSean@gmail.com>
2026-01-20 12:06:29 -0500

    Made the power bar work correctly

--- Assets/Gravity pull/Scripts/PowerBar.cs
+++ Assets/Gravity pull/Scripts/PowerBar.cs
@@ -3,10 +3,11 @@ using UnityEngine.InputSystem;
 
 public class PowerBar : MonoBehaviour
 {
-    public Transform startPos, endPos;
+    public Transform startPos, endPos; //The start and end points of the Lerp
     public Transform playerPos;
 
     public float time = 0;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -16,9 +17,15 @@ public class PowerBar : MonoBehaviour
     void Update()
     {
         //Debug.Log(((Vector2)playerPos.position - Mouse.current.position.ReadValue()).magnitude);
-        Debug.Log((Vector2)playerPos.position);
-        Debug.Log(Mouse.current.position.ReadValue());
-        time = (Camera.main.WorldToScreenPoint(Mouse.current.position.ReadValue()) - Camera.main.WorldToScreenPoint(playerPos.position)).magnitude;
-        transform.position = Vector2.Lerp(startPos.position, endPos.position, time);
+        //Debug.Log(Camera.main.WorldToScreenPoint((Vector2)playerPos.position));
+        //Debug.Log(Mouse.current.position.ReadValue());
+
+        //Time is euqal to the distance between the mouse's position and the player's position (in world space measurements)
+        time = ((Vector2)Camera.main.ScreenToWorldPoint(Mouse.current.position.ReadValue()) - (Vector2)playerPos.position).magnitude;
+        //If time is out of the range of influence
+        if (time > 5)
+            time = 0;
+        //dividing time by 5 so that it's always between 0 and 1
+        transform.position = Vector2.Lerp(startPos.position, endPos.position, time / 5);    
     }
 }
