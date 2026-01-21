42b120b79c0891dc01ab674eeca1452464bd1b30
Sean Erren <ErrenSean@gmail.com>
2026-01-21 17:15:40 -0500

    Changing the background. Adding a visual finish spot to the background. Added shadow to player.

--- Assets/Gravity pull/Scripts/WinScreenText.cs
+++ Assets/Gravity pull/Scripts/WinScreenText.cs
@@ -7,7 +7,7 @@ public class WinScreenText : MonoBehaviour
 
     public AnimationCurve curve;
 
-    float time = 0, timeIncrement = 0.01f;
+    float time = 0, timeIncrement = 0.001f;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -18,18 +18,14 @@ public class WinScreenText : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        //If the win message reached the desired size
-        if (transform.localScale.x >= 1 && transform.localScale.y >= 1)
-        {
-            //If the player's x and y are greater than the BOTTOM LEFT corner
-            if (playerPos.position.x > cornerBL.position.x && playerPos.position.y > cornerBL.position.y
+        //If the player's x and y are greater than the BOTTOM LEFT corner
+        if (playerPos.position.x > cornerBL.position.x && playerPos.position.y > cornerBL.position.y
                 //If the player's x and y are smaller than the TOP RIGHT corner
                 && playerPos.position.x < cornerTR.position.x && playerPos.position.y < cornerTR.position.y)
-            {
-                //Assigning the scale to the time based on the curve
-                transform.localScale = new Vector2(curve.Evaluate(time), curve.Evaluate(time));
-                time += timeIncrement;
-            }
+        {
+            //Assigning the scale to the time based on the curve
+            transform.localScale = new Vector2(curve.Evaluate(time), curve.Evaluate(time));
+            time += timeIncrement;
         }
     }
 }
