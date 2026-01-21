5956df008ca9bc58c5cec1efe47e91c3c4d519f3
Sean Erren <ErrenSean@gmail.com>
2026-01-21 10:30:12 -0500

    Changing sprites (barrels) from an array, using a random number. Switching between sprites (sun and moon).

--- Assets/Practice/Scripts/SpriteChanger.cs
+++ Assets/Practice/Scripts/SpriteChanger.cs
@@ -5,6 +5,13 @@ public class SpriteChanger : MonoBehaviour
 {
     public SpriteRenderer spriteRenderer;
     public Color c;
+    public Sprite spriteMoon,spriteSun;
+
+    public Sprite[] barrels = new Sprite[3];
+
+    bool hasSpriteChanged = false;
+    int randomBarrel = 0;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -14,11 +21,28 @@ public class SpriteChanger : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Keyboard.current.anyKey.wasPressedThisFrame)
-            changeColor();
+        //Switch between sun and moon
+        if (Keyboard.current.digit1Key.wasPressedThisFrame)
+        {
+            if (hasSpriteChanged)
+                spriteRenderer.sprite = spriteMoon;
+            else
+                spriteRenderer.sprite = spriteSun;
+
+            hasSpriteChanged = !hasSpriteChanged;
+
+            //changeColor();
+        }
+
+        //Change barrel
+        if (Keyboard.current.digit2Key.wasPressedThisFrame)
+        {
+            changeToRandomBarrel();
+        }
 
         Vector2 mousePos = Camera.main.ScreenToWorldPoint(Mouse.current.position.ReadValue());
 
+        //Change color on mouse hover
         //spriteRenderer.sprite.bounds refers to the bounds of the sprite but at 0,0
         if (spriteRenderer.bounds.Contains(mousePos))
         {
@@ -34,5 +58,14 @@ public class SpriteChanger : MonoBehaviour
     {
         //Picking a random color to change the sprite to
        spriteRenderer.color = Random.ColorHSV();
+    }
+
+    //Picks a random barrel from the array
+    void changeToRandomBarrel()
+    {
+        //Generate a random number (corrosponding with the amount of barrels)
+        randomBarrel = Random.Range(0, barrels.Length);
+
+        spriteRenderer.sprite = barrels[randomBarrel];
     }
 }
